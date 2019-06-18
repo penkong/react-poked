c6ad1feb622d03e075a71354998f7df904f6c5fb
mazdak <nazemi.works@gmail.com>
2019-06-17 21:04:46 -0700

    render poki

--- src/PokCard.js
+++ src/PokCard.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './PokCard.css';
 
+
+const POK_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 class PokCard extends Component {
   render() {
+    const {name , id, type, exp} = this.props
+    let imgSrc = `${POK_API}${id}.png` 
+
     return (
       <div className="PokCard">
-        <h1>{this.props.name}</h1>
+        <h1>{name}</h1>
+        <img src={imgSrc} alt={name}/>
+        <div>Type: {type}</div>
+        <div>exp: {exp}</div>
       </div>
     );
   }
