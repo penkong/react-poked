189b5e681b9530b2b3357d9a31fef555c9602a7f
mazdak <nazemi.works@gmail.com>
2019-06-19 22:59:09 -0700

    make add todo

---
+++ TODO/src/components/TodoForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+  state = {task: ""};
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.createTodo(this.state);
+    this.setState({ task: "" }); //reset form after submit
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="task">New TODO</label>
+        <input 
+          id="task" 
+          type="text" 
+          placeholder="New TODO"
+          name="task"
+          value={this.state.task}
+          onChange={this.handleChange}  
+        />
+        <button>Add</button>
+      </form>
+    );
+  }
+}
+
+export default TodoForm;
--- TODO/src/components/TodoList.js
+++ TODO/src/components/TodoList.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
+import TodoForm from './TodoForm';
 
 class TodoList extends Component {
   state = { todos : [ {task: 'walk the fish'}, {task: 'Greem dream'}] };
+  createTodo = newTodo => {
+    this.setState({ todos: [...this.state.todos, newTodo ]});
+  }
   render() {
     const todos = this.state.todos.map(todo => {
       return <Todo task={todo.task} />
@@ -10,7 +14,7 @@ class TodoList extends Component {
     return (
       <div>
         <h1>TODO LIST!</h1>
-
+        <TodoForm createTodo={this.createTodo}/>
         <ul>{todos}</ul>
       </div>
     );
