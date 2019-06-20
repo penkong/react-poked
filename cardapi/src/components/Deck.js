6a759c99b6a63093cdca8e08dfb1e59f37ccb384
mazdak <nazemi.works@gmail.com>
2019-06-20 04:29:40 -0700

    fetch card success

--- cardapi/src/components/Deck.js
+++ cardapi/src/components/Deck.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+
+
+
+const baseUrl = `https://deckofcardsapi.com/api/deck`;
 class Deck extends Component {
-  state = { deck: null };
-  componentDidMount() {
-    const url = `https://deckofcardsapi.com/api/deck/new/shuffle`;
+  state = { deck: null, drawn: [] };
+
+  async componentDidMount() {
+    const deck = await axios.get(`${baseUrl}/new/shuffle/`);
+    this.setState({ deck: deck.data });
   }
-  
+  getCard = async () => {
+    const { deck_id } = this.state.deck;
+    try {
+      const cardUrl = `${baseUrl}/${deck_id}/draw/`;
+      const res = await axios.get(cardUrl);
+      if(!res.data.success) throw new Error("No cards left");
+      const card = res.data.cards[0];
+      this.setState(state => ({
+        drawn: [
+          ...state.drawn, 
+          {
+            id: card.code , 
+            image: card.image ,
+            name: `${card.value} of ${card.suit}`
+          }
+        ]
+      }));
+    } catch (err){
+      alert(err);
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Card dealer</h1>
+        <button onClick={this.getCard}>get Card!</button>
       </div>
     );
   }
