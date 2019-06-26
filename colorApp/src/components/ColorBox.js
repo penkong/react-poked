78a836b32fbd7cb231fccfb840614506a4b08b03
mazdak <nazemi.works@gmail.com>
2019-06-25 21:12:52 -0700

    style colors reusable

--- colorApp/src/components/ColorBox.js
+++ colorApp/src/components/ColorBox.js
@@ -3,14 +3,19 @@ import React, { Component } from 'react';
 
 
 class ColorBox extends Component {
+
   render() {
+    const { name, background } = this.props;
     return (
-      <div className="ColorBox" style={{background: this.props.background}}>
+      <div className="ColorBox" style={{background}}>
         {/* we click on this span to see more of that specific color */}
-        <span>
-          {this.props.name}
-        </span>
-        <span>MORE</span>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
