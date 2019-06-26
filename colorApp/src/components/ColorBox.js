f1c3fad1374a7aa7bc67159a515951c5065e80f0
mazdak <nazemi.works@gmail.com>
2019-06-25 22:48:11 -0700

    overlay done

--- colorApp/src/components/ColorBox.js
+++ colorApp/src/components/ColorBox.js
@@ -4,13 +4,25 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 class ColorBox extends Component {
-
+  state = {copied: false};
+  changeCopyState = () => {
+    this.setState({copied: true}, ()=> {
+      setTimeout(()=> this.setState({copied: false}), 2000);
+    });
+  }
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard onCopy={this.changeCopyState} text={background}>
         <div className="ColorBox" style={{background}}>
-          {/* we click on this span to see more of that specific color */}
+
+          <div className={`copy-overlay ${copied && "show"}`} style={{background}}></div>
+          <div className={`copy-message ${copied && "show"}`}>
+            <h1>copied!</h1>
+            <p>{background}</p>
+          </div>
+
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
