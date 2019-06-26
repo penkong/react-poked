45c6a15d35cea1d0681c2415bdcf0b8bd318a315
mazdak <nazemi.works@gmail.com>
2019-06-25 22:18:48 -0700

    use copyToClipboard cool tool

--- colorApp/src/components/ColorBox.js
+++ colorApp/src/components/ColorBox.js
@@ -1,5 +1,6 @@
 import './ColorBox.css';
 import React, { Component } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 
 class ColorBox extends Component {
@@ -7,16 +8,18 @@ class ColorBox extends Component {
   render() {
     const { name, background } = this.props;
     return (
-      <div className="ColorBox" style={{background}}>
-        {/* we click on this span to see more of that specific color */}
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background}>
+        <div className="ColorBox" style={{background}}>
+          {/* we click on this span to see more of that specific color */}
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <span className="see-more">More</span>
         </div>
-        <span className="see-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
