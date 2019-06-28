aef82569ff35aac7b0f4c0d157e7e79f508c3f4c
mazdak <nazemi.works@gmail.com>
2019-06-28 01:53:50 -0700

    add drawer for palette form component plus react-color package

--- colorApp/src/components/NewPaletteForm.js
+++ colorApp/src/components/NewPaletteForm.js
@@ -1,13 +1,191 @@
 import React, { Component } from 'react';
 
+// import clsx from 'clsx';
+// import { makeStyles, useTheme } from '@material-ui/core/styles';
+// import Drawer from '@material-ui/core/Drawer';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import List from '@material-ui/core/List';
+// import Typography from '@material-ui/core/Typography';
+// import Divider from '@material-ui/core/Divider';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+// import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+// import ListItemText from '@material-ui/core/ListItemText';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import MailIcon from '@material-ui/icons/Mail';
+
+import classnames from 'classnames';
+import { withStyles, makeStyles, withTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { ChromePicker } from 'react-color';
+
+const drawerWidth = 600;
+
+const styles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
 class NewPaletteForm extends Component {
-  render() {
+  // const classes = useStyles();
+  // const theme = useTheme();
+  // const [open, setOpen] = React.useState(false);
+  state = { open: false };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  }
+  
+  handleDrawerClose = () => {
+    this.setState({open: false });
+  }
+
+  renderAppBar(){
+    const { classes } = this.props;
+    const { open } = this.state;
     return (
-      <div>
-        <h1>new PaletteForm</h1>
-      </div>
+      <AppBar
+        position="fixed"
+        className={classnames(classes.appBar, {[classes.appBarShift]: open,})}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="Open drawer"
+            onClick={this.handleDrawerOpen}
+            edge="start"
+            className={classnames(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" noWrap>
+            React Colors
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+
+  renderDrawer(){
+    const { classes } = this.props;
+    const { open } = this.state;
+    return (
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{ paper: classes.drawerPaper,}}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={this.handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <Typography variant="h4">
+          Design your palette
+        </Typography>
+        <div>
+          <Button variant="contained" color="secondary">Clear Palette</Button>
+          <Button variant="contained" color="primary">Random Color</Button>
+        </div>
+        <ChromePicker color="purple" onChangeComplete={(newColor) => console.log(newColor)}/>
+        <Button variant="contained" color="primary">Add Color</Button>
+      </Drawer>
+    )
+  }
+
+  renderMain(){
+    const { classes } = this.props;
+    const { open } = this.state;
+    return (
+      <main
+        className={classnames(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+    )
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+    <div className={classes.root}>
+      <CssBaseline />
+      {this.renderAppBar()}
+      {this.renderDrawer()}
+      {this.renderMain()}
+    </div>
     );
   }
 }
 
-export default NewPaletteForm;
+export default withStyles(styles, {withTheme: true})(NewPaletteForm);
