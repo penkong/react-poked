ad516ad061e45add8535f1c9ae7f04ac991b4a3a
mazdak <nazemi.works@gmail.com>
2019-06-27 21:55:22 -0700

    full refactor box to styled components

--- colorApp/src/components/ColorBox.js
+++ colorApp/src/components/ColorBox.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import chroma from 'chroma-js';
 import { withStyles } from "@material-ui/styles";
-import './ColorBox.css';
+// import './ColorBox.css';
 
 
 const styles = {
@@ -59,6 +59,66 @@ const styles = {
     cursor: "pointer",
     textDecoration: "none",
     opacity: "0"
+  },
+  boxContent: {
+    position: "absolute",
+    padding: "10px",
+    width: "90%",
+    left: "0",
+    bottom: "0",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.8s ease-in-out",
+    transform: "scale(0.1)",
+  },
+  showOverlay: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "100",
+    position: "absolute",
+  },
+  copyMessage: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    top: "0",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "4rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "600",
+      textShadow: "1px 1px gray",
+      /* background: rgba(255,255,255,0.3")", */
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "200",
+    }
+  },
+  showCpMsg: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "101",
+    transition: "all .9s ease-in-out",
+    transitionDelay: ".2s",
   }
 }
 
@@ -81,15 +141,15 @@ class ColorBox extends Component {
         <div className={classes.colorBox} style={{background}}>
 
           {/* these will show on onCopy */}
-          <div className={`copy-overlay ${copied && "show"}`} style={{background}}></div>
-          <div className={`copy-message ${copied && "show"}`}>
+          <div className={`${classes.copyOverlay} ${copied && classes.showOverlay}`} style={{background}}></div>
+          <div className={`${classes.copyMessage} ${copied && classes.showCpMsg}`}>
             <h1>copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
 
           {/* these are all text on default showing */}
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>Copy</button>
