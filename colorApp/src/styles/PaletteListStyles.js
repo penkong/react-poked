f4307312e746c248af1d4358c652ae461070cfde
mazdak <nazemi.works@gmail.com>
2019-06-30 02:30:47 -0700

    make land page responsive

--- colorApp/src/styles/PaletteListStyles.js
+++ colorApp/src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export default  {
   root: {
     backgroundColor: "lightblue",
@@ -13,6 +15,12 @@ export default  {
     alignItems: "flex-start",
     flexDirection: "column",
     flexWrap: "wrap",
+    [sizes.down("lg")]: {
+      width: "80%",
+    },
+    [sizes.down("sm")]: {
+      width: "70%",
+    }
   },
   nav: {
     display: "flex",
@@ -30,6 +38,13 @@ export default  {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%",
+    gridGap: "1.5rem",
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+    },
+    [sizes.down("xs")]: {
+      gridGap: "1rem",
+      gridTemplateColumns: "repeat(1, 100%)",
+    },
   }
 }
--- colorApp/src/styles/sizes.js
+++ colorApp/src/styles/sizes.js
@@ -17,6 +17,7 @@ export default {
       sm: "767.98px",
       md: "991.98px",
       lg: "1199.98px", //means lg size and below till md or where ever we defined
+      xl: "1400.98px", //
     }
     return `@media (max-width: ${sizes[size]})`
   }
