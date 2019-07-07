fc036d38e9a8ba2d6a499a8765ae8b545dd4cfbe
mazdak <nazemi.works@gmail.com>
2019-07-07 04:50:18 -0700

    make context work

--- contexed/src/components/Navbar.js
+++ contexed/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 import { withStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar';
@@ -11,12 +12,13 @@ import SearchIcon from '@material-ui/icons/Search';
 import styles from '../styles/NavbarStyles';
 
 class Navbar extends Component {
-
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <AppBar position='static' color='primary'>
+        <AppBar position='static' color={isDarkMode ? "default": "primary"}>
           <Toolbar>
             <IconButton className={classes.menuButton} color='inherit'>
               <span>fr</span>
@@ -24,7 +26,7 @@ class Navbar extends Component {
             <Typography className={classes.title} variant='h6' color='inherit'>
               App Title
             </Typography>
-            <Switch/>
+            <Switch onChange={toggleTheme}/>
             <div className={classes.grow}/>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
--- contexed/src/contexts/ThemeContext.js
+++ contexed/src/contexts/ThemeContext.js
@@ -4,9 +4,12 @@ export const ThemeContext = createContext();
 
 export class ThemeProvider extends Component {
   state = {isDarkMode: true};
+  toggleTheme = () => {
+    this.setState({isDarkMode: !this.state.isDarkMode });
+  }
   render() {
     return (
-      <ThemeContext.Provider value={{...this.state, tasteLikeChicken: true}}>
+      <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
         {this.props.children}
       </ThemeContext.Provider>
     )
