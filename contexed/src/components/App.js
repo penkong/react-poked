c4563733363826769e92fd631b86c658089d1f8a
mazdak <nazemi.works@gmail.com>
2019-07-07 01:05:52 -0700

    make navbar

---
+++ contexed/src/components/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+
+function App(){
+  return (
+    <Navbar/>
+  )
+}
+
+export default App;
---
+++ contexed/src/hooks/useToggleState.js
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+export default (initialVal = false) => {
+  const [state, setState] = useState(initialVal);
+  const toggle = () => setState(!state);
+  return [state, toggle];
+}
